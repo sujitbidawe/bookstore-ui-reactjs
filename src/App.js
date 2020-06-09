e71724cd9fd3e89b6c43cb16d9afdf7940f825f9
sujitbidawe <bidawesujit@gmail.com>
2020-06-09 17:11:17 +0530

    refactored: divisions and containers. added: footer

--- src/App.js
+++ src/App.js
@@ -1,32 +1,37 @@
 import React from 'react';
 import './App.css';
 import Navigation from './Navigation';
-import About from './About';
+import Wishlist from './Wishlist';
 import Home from "./Home";
 import Cart from "./Cart";
-
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="App">
-        <div className="App-header">
-          <h2 color="red">
-            BookStore
-        </h2>
-        </div>
-        <br />
-        <div className="PageTest">
-            <Switch>
-              <Route path="/" component={Home} exact />
-              <Route path="/cart" component={Cart} exact />
-              <Route path="/About" component={About} exact />
-            </Switch>
-            <Navigation />
-          </div>
+
+    <div className="App" >
+      
+      <div className="App-header">
+        <Navigation />
       </div>
-    </BrowserRouter>
+      
+      <div className="PageRoute">
+        <BrowserRouter>
+          <Switch>
+            <Route path="/" component={Home} exact />
+            <Route path="/cart" component={Cart} exact />
+            <Route path="/Wishlist" component={Wishlist} exact />
+          </Switch>
+        </BrowserRouter>
+      </div>
+
+      <footer className="footer">
+        <text is="x3d">
+          Copyright &#xf1f9; 2020, Bookstore Private Limited. All Rights Reserved
+        </text>
+      </footer>
+    
+    </div>
   );
 }
 
