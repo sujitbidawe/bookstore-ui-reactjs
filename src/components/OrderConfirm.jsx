ead4e721281ebc816f3d7a52f046e356b1a5e7d8
sujitbidawe <bidawesujit@gmail.com>
2020-06-16 09:56:06 +0530

    added: image to the page

--- src/components/OrderConfirm.jsx
+++ src/components/OrderConfirm.jsx
@@ -5,14 +5,19 @@ export default class OrderConfirm extends Component {
     render() {
         return (
             <div className="OrderConfirm">
-                <div><h2>
-                    Order Placed Successfully!
-                </h2>
+                <div className="orderPlaced">
+                    
+                    <h2>
+                        Order Placed Successfully!
+                    </h2>
+                <img src="http://www.pngall.com/wp-content/uploads/1/Celebration-PNG-Transparent-HD-Photo.png">
+
+                </img>
                 </div>
                 <p>Hurray! Your order is confirmed.</p>
                 <p>The order ID is #123456, save the order ID</p>
                 <p>for further communication.</p>
-                <table style={{width:"60%", border:"1px groove grey"}}>
+                <table style={{width:"60%", border:"0.5px groove grey"}}>
                     <tr >
                         <th >Email us</th>
                         <th >Contact us</th>
@@ -21,7 +26,7 @@ export default class OrderConfirm extends Component {
                     <tr>
                         <td style={{border:"1px groove grey"}}>test@mail.com</td>
                         <td style={{border:"1px groove grey"}}>0009990000</td>
-                        <td style={{border:"1px groove grey"}}>Chembure, Mumbai</td>
+                        <td style={{border:"1px groove grey"}}>Chembur, Mumbai</td>
                     </tr>
                 </table>
                 <div className="continueButton">
