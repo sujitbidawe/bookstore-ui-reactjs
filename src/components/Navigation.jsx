08d12975f1ebd12592686380ff92fd3178932785
sujitbidawe <bidawesujit@gmail.com>
2020-06-16 16:01:19 +0530

    refactored: moved css of cart button of navigation bar to css file

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -8,7 +8,7 @@ export default class Navigation extends Component {
                 <a href="/" style={{ color: "white", fontSize: "25px", textDecoration: 'none' }}> &#xf02d; BookStore</a>
                 <input style={{ fontFamily: "fontawesome", width: "450px", height: "25px", fontSize: "15px" }} placeholder="&#xf002;  Search..."></input>
                 <div>
-                    <a className="cartIcon" href="/Cart" style={{ color: "white", textDecoration: 'none', fontFamily: "fontawesome" }}><Badge color="primary" badgeContent={3} showZero></Badge>&#xf07a;Cart</a>
+                    <a className="cartIcon" href="/Cart" ><Badge color="primary" badgeContent={2} showZero></Badge>&#xf07a;Cart</a>
                     <a href="/wishlist" style={{ color: "white", textDecoration: 'none' }}><Badge color="primary" badgeContent={5} showZero></Badge>   &#xf08A;WishList</a>
                 </div>
             </li>
