532ffcca4190df899fc6c3c2598101e11e2cb41a
sujitbidawe <bidawesujit@gmail.com>
2020-06-18 16:05:08 +0530

    refactored: tag for order confirm text

--- src/components/OrderConfirm.jsx
+++ src/components/OrderConfirm.jsx
@@ -13,9 +13,10 @@ export default class OrderConfirm extends Component {
                 <div className="orderPlaced">
                     <img src={Order} alt=""></img>
                 </div>
-                <p>Hurray! Your order is confirmed.</p>
-                <p>The order ID is #123456, save the order ID</p>
-                <p>for further communication.</p>
+                <text>Hurray! Your order is confirmed.</text>
+                <text>The order ID is #123456, save the order ID</text>
+                <text>for further communication.</text>
+                <br></br>
                 <table style={{ width: "60%", border: "0.5px groove grey" }}>
                     <tr >
                         <th >Email us</th>
