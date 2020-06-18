0df03ae102b2f6a915fc41d006b3b5b9b6d8c0fd
sujitbidawe <bidawesujit@gmail.com>
2020-06-18 11:13:38 +0530

    refactored: html to add spacing between navigation buttons

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -8,8 +8,8 @@ export default class Navigation extends Component {
                 <a href="/" style={{ color: "white", fontSize: "25px", textDecoration: 'none' }}> &#xf02d; BookStore</a>
                 <input style={{ fontFamily: "fontawesome", width: "450px", height: "25px", fontSize: "15px" }} placeholder="&#xf002;  Search..."></input>
                 <div>
-                    <a className="cartIcon" href="/Cart" ><Badge color="primary" badgeContent={2} showZero></Badge>&#xf07a;Cart</a>
-                    <a href="/wishlist" style={{ color: "white", textDecoration: 'none' }}><Badge color="primary" badgeContent={5} showZero></Badge>   &#xf08A;WishList</a>
+                    <a className="cartIcon" href="/Cart" ><Badge color="primary" badgeContent={2} showZero></Badge>&#xf07a;  Cart</a>
+                    <a href="/wishlist" style={{ color: "white", textDecoration: 'none' }}><Badge color="primary" badgeContent={5} showZero></Badge>   &#xf08A;  WishList</a>
                 </div>
             </li>
         )
