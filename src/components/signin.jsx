8a2783ccd255c84b8194a13ae528e1ad54eaafa8
sujitbidawe <bidawesujit@gmail.com>
2020-06-25 15:46:05 +0530

    added: functionality for signing user in

--- src/components/signin.jsx
+++ src/components/signin.jsx
@@ -1,18 +1,46 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom';
+import BookData from './BookDataLayer';
+
+var bookData = new BookData();
 
 export class SignIn extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            username: '',
+            password: ''
+        }
+    }
+
+    handleChangeUsername = (e) => {
+        this.setState({
+            username: e.target.value
+        })
+    }
+
+    handleChangePassword = (e) => {
+        this.setState({
+            password: e.target.value
+        })
+    }
+
+    handleChangeLogin = async () => {
+        await bookData.signInData(this.state.username, this.state.password)
+        console.log("token:", localStorage.getItem("token"));
+    }
+
     render() {
         return (
             <div className="login-box">
                 <h1>Welcome to BookStore</h1>
                 <div style={{ padding: "10px", display: "flex", flexDirection: "column" }}>
-                    <input style={{ padding: "10px", margin: "5px" }} placeholder="Username"></input>
-                    <input style={{ padding: "10px", margin: "5px" }} placeholder="Password"></input>
+                    <input style={{ padding: "10px", margin: "5px" }} placeholder="Username" onChange={(e) => this.handleChangeUsername(e)}></input>
+                    <input style={{ padding: "10px", margin: "5px" }} placeholder="Password" onChange={(e) => this.handleChangePassword(e)}></input>
                 </div>
                 <div>
                     <Link to="/" >
-                        <button className="button">Login</button>
+                        <button className="button" onClick={this.handleChangeLogin}>Login</button>
                     </Link>
                 </div>
                 <div style={{ padding: "10px", margin:"5px", display: "flex", flexDirection: "column", fontSize:"20px", justifyContent:"center", alignItems:"center" }}>
