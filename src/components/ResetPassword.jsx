9136de5276759a3606b369e85ef13fe921cd57ce
sujitbidawe <bidawesujit@gmail.com>
2020-06-26 23:06:31 +0530

    added: functionality for reset password request

--- src/components/ResetPassword.jsx
+++ src/components/ResetPassword.jsx
@@ -1,18 +1,38 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom';
+import BookData from './BookDataLayer';
+
+var bookData = new BookData();
 
 export class ResetPassword extends Component {
+        constructor(props) {
+            super(props)
+            this.state = {
+                email: '',
+            }
+        }
+    
+        handleSetEmail = async (e) => {
+            await this.setState({
+                email: e.target.value
+            })
+        }
+
+        handleResetPassword = async () => {
+            await bookData.resetPassword(this.state.email)
+        }
+    
     render() {
         return (
             <div className="login-box">
                 <h1>Welcome to BookStore</h1>
                 <div style={{ padding: "10px", display: "flex", flexDirection: "column", alignItems:"center" }}>
                     <h4>Trouble logging in?</h4>
-                    <input style={{ padding: "10px", margin: "5px", width:"200px" }} placeholder="Email"></input>
+                    <input style={{ padding: "10px", margin: "5px", width:"200px" }} placeholder="Email" onChange={(e) => this.handleSetEmail(e)}></input>
                 </div>
                 <div>
                     <Link >
-                        <button className="button">Submit</button>
+                        <button className="button" onClick={this.handleResetPassword}>Submit</button>
                     </Link>
                 </div>
                 <div style={{ padding: "10px", margin:"5px", display: "flex", flexDirection: "column", fontSize:"20px", justifyContent:"center", alignItems:"center" }}>
